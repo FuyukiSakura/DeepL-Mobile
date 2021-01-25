7692731fe03752442909cefb5c710c9164d4c28c
Eric Lam <ericlamyauping@gmail.com>
2021-01-25 23:58:48 +1000

    copy the selected text to clipboard when using the DeepL selection toolbar button

--- DeepLMobile/DeepLMobile.Android/MainActivity.cs
+++ DeepLMobile/DeepLMobile.Android/MainActivity.cs
@@ -2,6 +2,7 @@
 using Android.Content.PM;
 using Android.OS;
 using Android.Runtime;
+using Xamarin.Essentials;
 
 namespace Utosoft.DeepLMobile.Droid
 {
@@ -17,9 +18,26 @@ namespace Utosoft.DeepLMobile.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            var text = Intent.GetStringExtra("selectedText");
+
+            LoadSelectedTextAsync();
             LoadApplication(new global::Utosoft.DeepLMobile.App());
         }
+
+        /// <summary>
+        /// Loads the selected text if app is launched by selection toolbar
+        /// </summary>
+        private async void LoadSelectedTextAsync()
+        {
+            var text = Intent!.GetStringExtra("selectedText");
+            if (!string.IsNullOrWhiteSpace(text))
+            {
+                await Clipboard.SetTextAsync(text);
+            }
+        }
+
+        ///
+        /// <inheritdoc />
+        ///
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
--- DeepLMobile/DeepLMobile/MainPage.xaml.cs
+++ DeepLMobile/DeepLMobile/MainPage.xaml.cs
@@ -10,15 +10,5 @@ namespace Utosoft.DeepLMobile
         {
             InitializeComponent();
         }
-
-        /// <summary>
-        /// Handles selected text display on tapped event
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private async void SelectedTextDisplay_OnTapped(object sender, EventArgs e)
-        {
-            await Clipboard.SetTextAsync(SelectedTextDisplay.Text);
-        }
     }
 }
