549a04dd31f4ee57e1afcb704484c5d0c6ed1679
Eric Lam <ericlamyauping@gmail.com>
2021-01-25 22:48:41 +1000

    add DeepL text selection toolbar

--- DeepL-Mobile/DeepL-Mobile.Android/MainActivity.cs
+++ DeepL-Mobile/DeepL-Mobile.Android/MainActivity.cs
@@ -1,15 +1,12 @@
-﻿using System;
-
+﻿
 using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace DeepL_Mobile.Droid
 {
-    [Activity(Label = "DeepL_Mobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "DeepL", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -21,6 +18,7 @@ namespace DeepL_Mobile.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            var text = Intent.GetStringExtra("selectedText");
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
---
+++ DeepL-Mobile/DeepL-Mobile.Android/TranslateActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+
+namespace DeepL_Mobile.Droid
+{
+    [Activity(Label = "DeepL", NoHistory = true)]
+    [IntentFilter(new [] { Intent.ActionProcessText },
+        Categories = new []
+        {
+            Intent.CategoryDefault
+        },
+        DataMimeType = "text/plain")]
+    public class TranslateActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            var selectedText = Intent.GetCharSequenceExtra(Intent.ExtraProcessText);
+            var intent = new Intent(this, typeof(MainActivity));
+            intent.PutExtra("selectedText", selectedText);
+            StartActivity(intent);
+        }
+    }
+}
